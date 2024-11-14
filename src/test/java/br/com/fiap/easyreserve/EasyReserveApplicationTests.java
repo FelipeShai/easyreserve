7cd0404d12c21848ea0d76212b9fd72a37e21427
Felipe Shai <felipeshai@hotmail.com>
2024-11-14 00:43:28 -0300

    feat: estrutura base do projeto

---
+++ src/test/java/br/com/fiap/easyreserve/EasyReserveApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.easyreserve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyReserveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
