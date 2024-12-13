f0c73737074a838ae424540a5469ab7823f3f6f0
yago <yagos-s@live.com>
2024-12-13 13:56:00 -0300

    - Adicionados testes unitários para o `RestaurantJpaGateway`, cobrindo:   - Criação de restaurantes (save)   - Exclusão por ID (deleteById)   - Busca por ID (findById)   - Listagem de todos os restaurantes (findAll)   - Atualização de restaurantes   - Cenário de ID inexistente   - Restrições de unicidade (comentado)   - Logs e prints otimizados
    - Adicionados testes unitários para o `ReservationJpaGateway`, cobrindo:
      - Criação de reservas (save)
      - Exclusão por ID (deleteById)
      - Busca por ID (findById)
      - Busca de reservas por restaurante (findByRestaurantId)
      - Busca de reservas por cliente (findByCustomerName)
      - Cenário de ID inexistente
      - Exclusão segura para IDs inexistentes
    
    - Adicionados testes unitários para o `ReviewJpaGateway`, cobrindo:
      - Criação de avaliações (save)
      - Exclusão por ID (deleteById)
      - Busca por ID (findById)
      - Busca de avaliações por restaurante (findByRestaurantId)
      - Cenário de ID inexistente
      - Exclusão segura para IDs inexistentes
    
    - Uso de `@Transactional` para isolar transações durante os testes
    - Refatoração de métodos de geração de dados para evitar redundância (`generateRestaurant`, `generateReservation`, `generateReview`)

--- src/main/java/br/com/fiap/easyreserve/domain/Reservation.java
+++ src/main/java/br/com/fiap/easyreserve/domain/Reservation.java
@@ -2,10 +2,12 @@ package br.com.fiap.easyreserve.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 
 @Getter
+@Setter
 @AllArgsConstructor
 public class Reservation {
         private Long id; // Identificador único
